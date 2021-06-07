23109c06d099a77ed88dc2b76ead212b8c0f8ab1
Niall <NiallMCGithub@gmail.com>
2021-06-07 15:22:31 +0100

    USE LOCALS

--- Request Item.lua
+++ Request Item.lua
@@ -1,20 +1,18 @@
 local tArgs = {...}
-local chest = peripheral.wrap(
-    "ironchest:diamond_chest_5"
-)
+local chest = peripheral.wrap("ironchest:diamond_chest_5")
 local controllers = table.pack({
-    peripheral.find("storagedrawers:controller") 
+    peripheral.find("storagedrawers:controller")
 })[1]
-        
+
 local item = string.lower(tostring(tArgs[1]))
 local amount = tonumber(tArgs[2])
- 
+
 for key, drawer in pairs(controllers) do
   local list = drawer.list()
   for i = 2, #list do
     local drawerItem = list[i]
     if(drawerItem) then
-        drawerItemName = drawerItem.name:gsub("_", ""):lower()
+        local drawerItemName = drawerItem.name:gsub("_", ""):lower()
         if(drawerItemName:find(item)) then
           chest.pullItems(peripheral.getName(drawer), i, amount)
         end
