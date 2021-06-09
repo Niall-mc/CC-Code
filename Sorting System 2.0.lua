3efa23bc498302449f98ae25e4866f9068ea98ec
Niall <NiallMCGithub@gmail.com>
2021-06-09 01:07:01 +0100

    Revert "Remove unnecessary checks"
    This reverts commit 631f401af266523a840001e5d7ea2596f4cebb61.

--- Sorting System 2.0.lua
+++ Sorting System 2.0.lua
@@ -4,35 +4,32 @@ local controllers = table.pack(peripheral.find("storagedrawers:controller"))
 local chests = table.pack(peripheral.find("ironchest:obsidian_chest"))
 
 local magicLocation = peripheral.getName(chests[1])
-local magicFilter = {"nouveau", "astral", "eidolon", "rune", "tome", "enchanted", "potion", "forbidden", "artifacts", "mana",
-                    "dragon_breath", "ender_air", "charm", "brewing"}
+local magicFilter = {"nouveau", "astral", "eidolon", "rune", "tome", "enchanted", "potion", "forbidden", "artifacts"}
 
 local immersiveLocation = peripheral.getName(chests[2])
 local immersiveFilter = {"immersive", "tetra"}
 
 local someMachinesLocation = peripheral.getName(chests[3])
-local machineFilter = {"create", "foregoing", "computercraft", "enderstorage", "fluxnetworks", "mekanism", "darkutils", "itemcollector"}
+local machineFilter = {"create", "foregoing", "computercraft", "enderstorage", "fluxnetworks", "mekanism"}
 
 local organicsLocation = peripheral.getName(chests[4])
 local organicItemsFilter = {"petal", "dye", "flower", "minecraft:flowers", "mushroom", "head", "tear", "shell",
                             "kelp", "bat_wing", "weed", "seed", "skull", "cactus", "alex", "web", "venison",
-                            "apple", "lotus", "mud", "carapace", "forge:slimeballs", "chitin", "mysticalworld:proteins",
-                            "silkworm", "neapolitan", "sapling", "vine", "fungus", "stem", "farmers", "minecraft:crops", "forge:seeds",
-                            "leaf", "jelly", "antlers", "root", "fruit", "melon", "fish", "minecraft:fishes", "forge:grains", "forge:crops"}
+                            "neapolitan", "sapling", "vine", "fungus", "stem", "farmers", "minecraft:crops", "forge:seeds", "forge:grains", "forge:crops"}
 
 local woodItemsLocation = peripheral.getName(chests[5])
 local woodItemsFilter = {
     "scaffold", "sign", "minecraft:wooden_fences", "crafting_table",
     "drawer", "bowl", "carpentry", "boat", "minecraft:wooden_doors", "rail", "pattern",
     "ladder", "plank", "minecraft:logs", "beehive", "item_frame", "forge:chests/wooden", "minecraft:wooden_slabs",
-    "minecraft:wooden_stairs", "minecraft:wooden_trapdoors", "forge:fence_gates/wooden", "minecraft:wooden_buttons", "pestle", "composter"}
+    "minecraft:wooden_stairs", "minecraft:wooden_trapdoors", "minecraft:wooden_buttons", "pestle"}
 
 local toolsArmourLocation = peripheral.getName(chests[6])
 local toolsArmourFilter = {"ironchest", "armor", "armour", "helm", "chestplate", "leggings", "boots", "axe", "sword", "shovel",
-                     "shears", "hoe", "hood", "disc", "bucket", "bow", "flint_and_steel", "xercamusic", "saddle", "hat"}
+                     "hoe", "hood", "disc", "bucket", "bow", "flint_and_steel", "xercamusic", "saddle", "hat"}
 
 local stoneStuffLocation = peripheral.getName(chests[7])
-local stoneStuffFilter = {"stone", "furnace", "observer", "dispenser", "smoker", "smithing"}
+local stoneStuffFilter = "stone"
 
 local locationsMap = {
     [magicLocation] = magicFilter,
@@ -46,22 +43,23 @@ local locationsMap = {
 
 local function moveItem(item, slot)
     local itemName = item.name
-    local itemCount = item.count
     local tags = item.tags
     for i = 1, #controllers do
         local amount = ender.pushItems(peripheral.getName(controllers[i]), slot)
         if amount > 0 then
-            if amount == itemCount then
-                return itemCount
+            if amount == item.count then
+                return item.count
             end
             print("Sending " .. itemName .. " to overflow.")
             i = #controllers
         end
     end
     for k, v in pairs(locationsMap) do
-        for i = 1, #v do
-            if itemName:find(v[i]) or tags[v[i]] then
-                return ender.pushItems(k, slot)
+        if type(v) == "table" then
+            for i = 1, #v do
+                if itemName:find(v[i]) or tags[v[i]] then
+                    return ender.pushItems(k, slot)
+                end
             end
         end
     end
@@ -71,11 +69,9 @@ end
 local function moveItems()
     for i = 1, ender.size() do
         local item = ender.getItemDetail(i)
-        local itemName = item.name
-        local itemCount = item.count
         if item then
-            if moveItem(item, i) < itemCount then
-                print("No space for: " .. itemName)
+            if moveItem(item, i) < item.count then
+                print("No space for: " .. item.name)
             end
         end
     end
